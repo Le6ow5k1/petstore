a30de2b4d3624d50ae09670e03a5daaac380fb42
Le6ow5k1 <bebot@mail.ru>
2014-11-23 19:00:13 +0500

    Create Pets resource.

---
+++ lib/petstore/resources/pets.rb
@@ -0,0 +1,19 @@
+require 'petstore/resource'
+
+module Petstore
+  module Resources
+    class Pets < Petstore::Resource
+      set_path 'pet'
+
+      def find_by_status(status)
+        Petstore::Request.new(conn: conn, partial_path: partial_path('findByStatus'),
+                              method: :get, params: {status: status.to_s}).make
+      end
+
+      def find_by_tags(tags)
+        Petstore::Request.new(conn: conn, partial_path: partial_path('findByTags'),
+                              method: :get, params: {tags: "#{tags.join(',')}"}).make
+      end
+    end
+  end
+end
