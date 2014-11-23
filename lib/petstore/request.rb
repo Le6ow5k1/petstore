3170a3fafbb9bb091a42ced1fc553ae9a1910ef3
Le6ow5k1 <bebot@mail.ru>
2014-11-23 15:32:40 +0500

    Add common HTTP methods.

--- lib/petstore/request.rb
+++ lib/petstore/request.rb
@@ -1,20 +1,55 @@
 module Petstore
   class Request
     require 'active_model'
+
     include ActiveModel::Validations
 
-    attr_reader :base_uri, :partial_path, :method, :params
+    attr_reader :conn, :partial_path, :method, :params
 
-    validates_presence_of :base_uri, :partial_path, :method
+    validates_presence_of :conn, :partial_path, :method
 
     def initialize(options = {})
-      @base_uri = options[:base_uri]
+      @conn = options[:conn]
       @partial_path = options[:partial_path]
       @method = options[:method]
       @params = options[:params]
     end
     
-    
+    def make
+      case @method
+      when :get
+        get
+      when :post
+        post
+      when :delete
+        delete
+      when :put
+        put
+      end
+    end
+
+    private
+
+    def get
+      @conn.get @partial_path, @params
+    end
+
+    def post
+      @conn.post @partial_path, @params do |req|
+        req.headers['Content-Type'] = 'application/json'
+      end
+    end
+
+    def delete
+      @conn.delete @partial_path, @params
+    end
+
+    def put
+      @conn.put @partial_path, @params do |req|
+        req.headers['Content-Type'] = 'application/json'
+      end
+    end
+
   end
   
 end
