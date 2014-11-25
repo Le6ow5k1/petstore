ba8aceba643721a2dfc95c8808a8d7acbe1e9d73
Le6ow5k1 <bebot@mail.ru>
2014-11-25 23:17:00 +0500

    Remove examples for #make and add for #mime_type.

--- spec/unit/request_spec.rb
+++ spec/unit/request_spec.rb
@@ -2,31 +2,24 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Petstore::Request do
   before(:each) do
-    @conn = Faraday.new do |builder|
-      builder.adapter :test do |stub|
-        stub.get('/pet') { |env| [ 200, {}, 'dog' ]}
-        stub.post('/pet') { |env| [ 200, {}, 'success' ]}
+    @request = Petstore::Request.new(conn: '', partial_path: '', method: '')
+  end
+
+  describe '#mime_type' do
+    context 'with filename with .jpg extension passed' do
+      let(:subject) { @request.send(:mime_type, 'file.name.jpg') }
+
+      it 'returns image/jpeg string' do
+        expect(subject).to eq('image/jpeg')
+      end
+    end
+
+    context 'with filename with .png extension passed' do
+      let(:subject) { @request.send(:mime_type, 'file .1.png') }
+
+      it 'returns image/png string' do
+        expect(subject).to eq('image/png')
       end
     end
   end
-
-  describe '#make' do
-    context 'on Request instance with method option equals get' do
-      subject { Petstore::Request.new(conn: @conn, partial_path: '/pet', method: :get) }
-
-      it 'does a get request' do
-        expect(subject.send(:get).body).to eq('dog')
-      end
-    end
-
-    context 'on Request instance with method option equals post' do
-      subject { Petstore::Request.new(conn: @conn, partial_path: '/pet', method: :post, params: {}) }
-
-      it 'does a post request' do
-        expect(subject.send(:post).body).to eq('success')
-      end
-    end
-
-  end
-
 end
