7378f354a219ba9154c25614c3d0d5dce695ef4b
Le6ow5k1 <bebot@mail.ru>
2014-11-26 22:13:34 +0500

    Little refactoring.

--- lib/petstore/response.rb
+++ lib/petstore/response.rb
@@ -11,22 +11,34 @@ module Petstore
     end
 
     def parse
+      parse_body
+
       case @status
       when 200
-        begin
-          JSON.parse(@body)
-        rescue JSON::ParserError
-          nil
-        end
+        @body
       when 404
-        raise NotFound.new @body['message']
+        raise NotFound.new body_message
       when 405
-        raise NotAllowed.new @body['message']
+        raise NotAllowed.new body_message
       when 400
-        raise InvalidRequest.new @body['message']
+        raise InvalidRequest.new body_message
       when 500
-        raise ServerError.new @body['message']
+        raise ServerError.new body_message
       end
+    end
+
+    private
+
+    def parse_body
+      begin
+        @body = JSON.parse(@body)
+      rescue JSON::ParserError
+        @body = nil
+      end
+    end
+
+    def body_message
+      @body['message'] if @body && @body.is_a?(Hash)
     end
 
   end
